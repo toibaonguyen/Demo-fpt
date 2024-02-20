e0153f7ab4368ce60fab3a6d2eb1bb35e4a3075e
Bao <toibaonguyen@gmail.com>
2024-02-20 10:20:16 +0700

    Update for easy read main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import OcrToTableTool as ottt
-import TableExtractor as te
-import TableLinesRemover as tlr
+from OcrToTableTool import OcrToTableTool
+from TableExtractor import TableExtractor
+from TableLinesRemover import TableLinesRemover
 import cv2
 
 path_to_image = "./images/OCR.jpg"
-table_extractor = te.TableExtractor(path_to_image)
+table_extractor = TableExtractor(path_to_image)
 perspective_corrected_image = table_extractor.execute()
 cv2.imshow("perspective_corrected_image", perspective_corrected_image)
 
-lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
+lines_remover = TableLinesRemover(perspective_corrected_image)
 image_without_lines = lines_remover.execute()
 cv2.imshow("image_without_lines", image_without_lines)
 
-ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
+ocr_tool = OcrToTableTool(image_without_lines, perspective_corrected_image)
 ocr_tool.execute()
 
 cv2.waitKey(0)
